e7fb637dbe43ccc9e9929d0ff834d835c7419acd
Clement Guichard <c.guichard508@laposte.net>
2022-07-28 17:33:06 +0200

    +maze gen in 2D & 3D scenes

---
+++ Assets/Game/Scripts/Runtime/ViewModels/Generation/MazeGenerator.cs
@@ -0,0 +1,56 @@
+using Project.Models.Maze;
+using Project.Procedural.MazeGeneration;
+using UnityEngine;
+
+namespace Project.ViewModels.Generation
+{
+    public class MazeGenerator : MonoBehaviour
+    {
+        [field: SerializeField] public CustomMazeSettingsSO Settings { get; set; }
+        public IDrawableGrid Grid { get; set; }
+        public IDrawMethod DrawMethod { get; set; }
+
+
+        [ContextMenu("Cleanup")]
+        public void Cleanup()
+        {
+            if (DrawMethod is not null)
+            {
+                DrawMethod.Cleanup();
+            }
+        }
+
+
+        [ContextMenu("Execute Generation Algorithm")]
+        public void Execute()
+        {
+            SetupGrid();
+            Generate();
+            Draw();
+
+        }
+
+
+        private void SetupGrid()
+        {
+            Grid = new ColoredGrid(Settings);
+        }
+
+        private void Generate()
+        {
+            IGeneration genAlg = InterfaceFactory.GetGenerationAlgorithm(Settings);
+            genAlg.Execute(Grid);
+
+            //TODO : Change this to start distances at Player position
+            Cell start = Grid[Grid.Rows / 2, Grid.Columns / 2];
+            Grid.SetDistances(start.GetDistances());
+
+        }
+
+        private void Draw()
+        {
+            DrawMethod = InterfaceFactory.GetDrawMode(Settings);
+            DrawMethod.Draw(Grid);
+        }
+    }
+}
