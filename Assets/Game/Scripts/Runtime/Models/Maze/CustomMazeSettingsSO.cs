8a2efcab5712fbeee02cf04126c1baaab8827108
Clement Guichard <c.guichard508@laposte.net>
2022-07-28 17:02:21 +0200

    +setup custom maze settings & project structure

---
+++ Assets/Game/Scripts/Runtime/Models/Maze/CustomMazeSettingsSO.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Project.Procedural.MazeGeneration;
+
+
+namespace Project.Models.Maze
+{
+    [CreateAssetMenu(fileName = "New Custom Maze Settings", menuName = "ScriptableObjects/Custom Maze Settings (Use this)", order = 1)]
+    public class CustomMazeSettingsSO : GenerationSettingsSO
+    {
+
+    }
+}
